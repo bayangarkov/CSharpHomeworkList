76acd50f0fe8cf0c69cda6e7f866997ed41fa5bd
bayangarkov <baiangarkov@gmail.com>
2017-02-22 20:52:36 +0200

    Removing element from list

---
+++ 02-RemoveElementsAtOddPosition/RemoveElementsAtOddPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_RemoveElementsAtOddPosition
+{
+    class RemoveElementsAtOddPosition
+    {
+        static void Main()
+        {
+            var inputWords = Console.ReadLine().Split(' ').ToList();
+
+            var outputWords = new List<string>();
+
+            for (int i = 0; i < inputWords.Count; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    outputWords.Add(inputWords[i]);
+                }
+            }
+
+            foreach (var words in outputWords)
+            {
+                Console.Write(string.Join("", words));
+            }
+            Console.WriteLine();
+        }
+    }
+}
