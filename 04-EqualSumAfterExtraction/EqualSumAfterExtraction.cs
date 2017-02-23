e00026eb3629f243d525ac2d73c97df67465f67d
bayangarkov <baiangarkov@gmail.com>
2017-02-23 21:31:39 +0200

    Comparing two lists of int and delete some values and sum them

---
+++ 04-EqualSumAfterExtraction/EqualSumAfterExtraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_EqualSumAfterExtraction
+{
+    class EqualSumAfterExtraction
+    {
+        static void Main()
+        {
+            var listOne = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var listTwo = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            
+            var secondWithoutFirst = listTwo.Except(listOne).ToList();
+
+           
+            if (listOne.Sum() == secondWithoutFirst.Sum() || listOne.Sum() == listTwo.Sum())
+            {
+                Console.WriteLine($"Yes. Sum: {listOne.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"No. Diff: {Math.Abs(listOne.Sum() - secondWithoutFirst.Sum())}");
+            }
+
+        }
+    }
+}
