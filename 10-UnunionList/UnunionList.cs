4a507eafbbc88e9c0261c588a333ae5db806bfd1
bayangarkov <baiangarkov@gmail.com>
2017-02-25 18:27:16 +0200

    Comparing several lists with one primal , and if we have more or less we delete or add to a primal list

---
+++ 10-UnunionList/UnunionList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_UnunionList
+{
+    class UnunionList
+    {
+        static void Main()
+        {
+            var primal = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var loopList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+                foreach (var num in loopList)
+                {
+                    if (primal.Contains(num))
+                    {
+                        primal.Remove(num);
+                    }
+                    else
+                    {
+                        primal.Add(num);
+                    }
+                }
+            }
+
+            primal.Sort();
+            Console.Write(string.Join(" ",primal));
+        }
+    }
+}
