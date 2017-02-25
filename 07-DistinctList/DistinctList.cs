6557a16fdda98e2b264c71fd59dc195de5419242
bayangarkov <baiangarkov@gmail.com>
2017-02-25 15:14:48 +0200

    Check if a number exist more than twice in one list and remove it

---
+++ 07-DistinctList/DistinctList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_DistinctList
+{
+    class DistinctList
+    {
+        static void Main()
+        {
+            var inputNums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var unique = inputNums.Distinct().ToList();
+
+            foreach (var num in unique)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
