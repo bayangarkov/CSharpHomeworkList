ad16ba65f58209c3342a81e0210f15cf18ada6dc
bayangarkov <baiangarkov@gmail.com>
2017-03-02 20:51:06 +0200

    Check how many times an item appears in list and count them

---
+++ 15-ArrayHistogram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ArrayHistogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split(' ').ToList();
+            var q = list.GroupBy(x => x)
+                        .Select(g => new { Value = g.Key, Count = g.Count() })
+                        .OrderByDescending(x => x.Count);
+            
+
+            foreach (var x in q)
+            {
+                var result = x.Count * 100.0 / list.Count;
+                Console.WriteLine(x.Value + " -> " + x.Count + " times" + " ({0:F2}%)",result);
+            }
+            
+        }
+    }
+}
